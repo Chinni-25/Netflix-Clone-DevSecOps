2b672411371bb1f6aa274416a870d2b65a271dbe
Kaizoku-ps <klaxmiprasanna25@gmail.com>
2026-07-05 20:00:33 +0530

    Attach EC2 key pair

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,12 +12,17 @@ resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ubuntu.id
   subnet_id              = aws_subnet.public-subnet[count.index].id
   instance_type          = var.ec2-instance-type[count.index]
+
+  key_name               = "Demo"
+
   iam_instance_profile   = aws_iam_instance_profile.iam-instance-profile.name
   vpc_security_group_ids = [aws_security_group.default-ec2-sg.id]
+
   root_block_device {
     volume_size = var.ec2-volume-size
     volume_type = var.ec2-volume-type
   }
+}
 
   tags = {
     Name = "${local.org}-${local.project}-${local.env}-${local.instance_names[count.index]}"
